84e5a82a5edb914b5cfedb853c4525fa7761ffc0
DharmishthabenParmar <dharmishthaben.parmar@dcmail.ca>
2023-11-24 22:10:47 -0500

    write the whole code
    write the whole code make changes where nessesorry

--- Lab4/Part3/mainfilePart3.js
+++ Lab4/Part3/mainfilePart3.js
@@ -1,23 +1,18 @@
 // set up canvas
-
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
-
-// function to generate random number
-
+// Function to generate a random number within a given range
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // function to generate random RGB color value
-
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
-
+// Class representing a ball object
 class Ball {
 
    constructor(x, y, velX, velY, color, size) {
@@ -28,14 +23,14 @@ class Ball {
       this.color = color;
       this.size = size;
    }
-
+   // Method to draw the ball on the canvas
    draw() {
       ctx.beginPath();
       ctx.fillStyle = this.color;
       ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
       ctx.fill();
    }
-
+   // Method to update the ball's position
    update() {
       if ((this.x + this.size) >= width) {
          this.velX = -(Math.abs(this.velX));
@@ -56,7 +51,8 @@ class Ball {
       this.x += this.velX;
       this.y += this.velY;
    }
-
+   // Method to check for collisions between balls and change their colors
+   
    collisionDetect() {
       for (const ball of balls) {
          if (!(this === ball)) {
@@ -72,9 +68,10 @@ class Ball {
    }
 
 }
-
+// Create an array to store ball objects
 const balls = [];
 
+// Generate 25 balls with random properties
 while (balls.length < 25) {
    const size = random(10,20);
    const ball = new Ball(
@@ -90,18 +87,18 @@ while (balls.length < 25) {
 
   balls.push(ball);
 }
-
+// Function to continuously update and render the animation
 function loop() {
    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
    ctx.fillRect(0, 0,  width, height);
-
+   // Draw, update, and check for collisions for each ball
    for (const ball of balls) {
      ball.draw();
      ball.update();
      ball.collisionDetect();
    }
-
+   // Request the next animation frame
    requestAnimationFrame(loop);
 }
-
+// Start the animation loop
 loop();
